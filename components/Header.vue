06f6ff0c90cb1006efcbeb314915017f8a1ac667
Carlos <carloseam94@gmail.com>
2023-02-06 10:37:15 +0100

    add: lawnotation logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-navbar type="dark" variant="dark">
+  <b-navbar type="light" variant="light">
     <b-navbar-nav>
-        <NuxtLink to="/" class="nav-link">Home</NuxtLink>
+        <NuxtLink to="/" class="nav-link"><img src="lawnotation-logo.svg" alt="logo" style="width: 200px"></NuxtLink>
     </b-navbar-nav>
   </b-navbar>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
   </template>
   <style>
   main {
-      min-height: calc(100vh - 92px);
+      min-height: calc(100vh - 110px);
       padding-bottom: 15px;
       padding-top: 15px;
   }
