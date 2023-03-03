5605956b89377432a9ff5da19336dc3ac1a1bbeb
eensander <50321538+eensander@users.noreply.github.com>
2023-03-03 16:48:50 +0100

    change: moved importing labels to above inputs on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
   <main>
     <div class="container">
+
       <div class="row">
+        <label>
+          <span  class="btn btn-success">Import labels</span>
+          <input type="file" style="display: none;" @change="import_labels_file_changed" />
+          <span v-if="$store.state.labels.length > 0">Currently have {{ $store.state.labels.length }} labels.</span>
+        </label>
+      </div>
+
+      <div class="row">
+
         <div class="col text-center">
           <h3>Upload text with annotations:</h3>
           <b-form-file id="file-small" size="sm" accept=".json" @change="change_file($event)"></b-form-file>
@@ -23,6 +33,30 @@ export default {
     }
   },
   methods: {
+    import_labels_file_changed(event) {
+      const reader = new FileReader();
+      reader.onload = (ev) => {
+        if ((ev.target?.result ?? false) && typeof ev.target?.result === 'string') {
+          try {
+            const import_raw = ev.target?.result;
+            const import_labels = JSON.parse(import_raw)['label_collection']['labels']
+            this.import_labels(import_labels)
+          } catch {
+            // TODO: toast/show error
+            alert("No labels found in file")
+          }
+        }
+      };
+      const event_target = event.target;
+      if (event_target.files != null) {
+          reader.readAsText(event_target.files[0]);
+      }
+    },
+    import_labels(labels) {
+      // this.labelStudio.createLabel()
+      // alert(`Parsed ${labels.length} labels: ${labels.map(x => x.name).join(", ")}`)
+      this.$store.commit('set_labels', labels);
+    },
     change_file(event) {
       var reader = new FileReader();
       reader.onload = () => {
